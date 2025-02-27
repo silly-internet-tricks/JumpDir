a4efda3e10a2c0fb740883bf92c4720c47baa11a
Josh Parkker <jparker@sherpas.ai>
2025-02-26 19:32:59 -0700

    try making the origin directory optional (and also gofmt)

--- main.go
+++ main.go
@@ -1,43 +1,55 @@
 package main
 
 import (
-	"fmt"
 	"flag"
+	"fmt"
 	"os"
+	"path/filepath"
+
 	"github.com/DNelson35/JumpDir/search"
 )
 
-
-
 func printUsage() {
-	    fmt.Println("Usage: go run main.go <target_directory> [<starting_point>]")
-    fmt.Println()
-    fmt.Println("Arguments:")
-    fmt.Println("  <target_directory>    Target directory (required)")
-    fmt.Println("  [<starting_point>]    Starting point (optional)")
-    fmt.Println()
-    fmt.Println("Flags:")
-    flag.PrintDefaults()
+	fmt.Println("Usage: go run main.go <target_directory> [<starting_point>]")
+	fmt.Println()
+	fmt.Println("Arguments:")
+	fmt.Println("  <target_directory>    Target directory (required)")
+	fmt.Println("  [<starting_point>]    Starting point (optional)")
+	fmt.Println()
+	fmt.Println("Flags:")
+	flag.PrintDefaults()
 }
 
-
-
-func main(){
+func main() {
 	var help bool
 	flag.BoolVar(&help, "help", false, "Show help information")
-	flag.BoolVar(&help, "h", false, "Show help information") 
+	flag.BoolVar(&help, "h", false, "Show help information")
 	flag.Parse()
-	
+
 	if help {
 		printUsage()
 		os.Exit(0)
 	}
-	
+
 	args := flag.Args()
-	if len(args) < 2 {
-		fmt.Println("Error: <target_directory> and <starting_point> are required.")
+	if len(args) < 1 {
+		fmt.Println("Error: <target_directory> is required.")
 		printUsage()
 		os.Exit(1)
+	}
+
+	// Note: this is trying to make the starting point optional.
+	// (since it said in the usage that it would be optional)
+	if len(args) < 2 {
+		// shoutout to: https://stackoverflow.com/a/18537419/27913035
+		ex, err := os.Executable()
+		if err != nil {
+			fmt.Println("an error occurred")
+			fmt.Println(err.Error())
+			os.Exit(1)
+		}
+
+		args = append(args, filepath.Dir(ex))
 	}
 
 	name := args[0]
@@ -45,6 +57,4 @@ func main(){
 
 	result := search.JumpDirectory(name, currDir)
 	fmt.Println(result)
-
 }
-
--- search/search.go
+++ search/search.go
@@ -1,34 +1,34 @@
 package search
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/fs"
 	"os"
 	"path/filepath"
 	"slices"
 	"strings"
-	"encoding/json"
 )
 
 //TODO: potentially add another flag to no use the ignore patterns / or maybe to ignore a particular pattern / or a flag to add and remove patterns from the config from the cli using the jd command. potentially also add in a way to parse the json for any list so users can organize patterns as they see fit.
 
 //TODO: possibly add a -c or something flag for continued search that will not stop on the first found search but instead return all matching dirs found.
 
-//TODO: maybe incorporate a markdown generator representing the files system. could be cool idea from boot.dev https://github.com/aymaneallaoui/dirscanner could be under jd -G starting dir found using the jd command
+// TODO: maybe incorporate a markdown generator representing the files system. could be cool idea from boot.dev https://github.com/aymaneallaoui/dirscanner could be under jd -G starting dir found using the jd command
 type Config struct {
 	IgnorePatterns []string `json:"ignore_patterns"`
 	RestrictedDirs []string `json:"restrictedDirs"`
 	Loaded         bool
 	Patterns       []string
 }
-	
+
 var config = &Config{}
 
 // LoadPatterns loads configuration from a JSON file specified by the CONFIG_PATH environment variable,
 // merging ignore patterns and restricted directories into the Patterns slice. It ensures patterns are
 // only loaded once and sets the Loaded flag to true upon success.
-func (c *Config) LoadPatterns()error{
-	if c.Loaded{
+func (c *Config) LoadPatterns() error {
+	if c.Loaded {
 		return nil
 	}
 
@@ -42,7 +42,7 @@ func (c *Config) LoadPatterns()error{
 	defer file.Close()
 
 	decoder := json.NewDecoder(file)
-	
+
 	if err := decoder.Decode(&config); err != nil {
 		return err
 	}
@@ -65,11 +65,11 @@ func JumpDirectory(name string, currDir string) string {
 	os.Chdir(currDir)
 	cleanPaths, path := getDirs(currDir, name)
 
-	if path != ""{
+	if path != "" {
 		return path
 	}
 
-	for _, absPath := range cleanPaths{
+	for _, absPath := range cleanPaths {
 		path = JumpDirectory(name, absPath)
 		if path != "" {
 			break
@@ -79,37 +79,36 @@ func JumpDirectory(name string, currDir string) string {
 	return path
 }
 
-
-func searchVisDirs(dirs []fs.DirEntry, name string)([]string, string) {
+func searchVisDirs(dirs []fs.DirEntry, name string) ([]string, string) {
 	var visDirs []string
 
-	for _, dir := range dirs{
+	for _, dir := range dirs {
 		if dir.Name()[0] == '.' || !dir.IsDir() || slices.Contains(config.Patterns, dir.Name()) {
 			continue
-		}else if checkMatch(name, dir){
+		} else if checkMatch(name, dir) {
 			path, _ := filepath.Abs(dir.Name())
 			return nil, path
 		}
 
-		if len(dirs) != 0{
+		if len(dirs) != 0 {
 			path, err := filepath.Abs(dir.Name())
 			if err != nil {
 				fmt.Println(err)
 			}
 			visDirs = append(visDirs, path)
 		}
-		
+
 	}
 	return visDirs, ""
 }
 
-func checkMatch(name string, dir fs.DirEntry)bool{	
-	return strings.EqualFold(dir.Name(), name ) 
+func checkMatch(name string, dir fs.DirEntry) bool {
+	return strings.EqualFold(dir.Name(), name)
 }
 
-func getDirs(dir string, name string)([]string, string){
+func getDirs(dir string, name string) ([]string, string) {
 	dirs, err := os.ReadDir(dir)
-	
+
 	if err != nil {
 		fmt.Println(err)
 	}
